89491ce04418bd235dc14c48a7d4b6b72b89ae91
ChiragQA1983 <chirag.bhatt105@gmail.com>
2026-06-11 10:04:05 +0530

    GetbyRole file

---
+++ Pages/Getbyrole.js
@@ -0,0 +1,39 @@
+import {expect} from '@playwright/test';
+
+export default class Getbyrole
+{
+   constructor(page)
+   {
+     this.page=page;
+     this.Buttons=page.getByRole('Button',{name: "Primary Action"});
+     this.togglebutton=page.getByRole('Button',{name:"Toggle Button"});
+     this.checkbox=page.getByRole('checkbox',{name:'Accept terms'});    
+     this.textbox=page.getByRole('textbox',{name:'Username:'})
+   }
+ 
+   async goto()
+   {
+       await this.page.goto("https://testautomationpractice.blogspot.com/p/playwrightpractice.html#")
+
+   }    
+ 
+   async getbyrolebuttonvalidation()
+   {
+    //Button       
+    await expect(this.Buttons).toBeVisible();
+    await expect(this.Buttons).toContainText("Primary Action")
+
+    //Button   
+    await expect(this.togglebutton).toBeVisible();
+    await expect(this.togglebutton).toContainText("Toggle Button");
+
+    //Checkbox
+    await this.checkbox.check();
+    await expect(this.checkbox).toBeChecked();
+ 
+    //Textbox
+    await this.textbox.fill("Chirag Bhatt");
+    await expect(this.textbox).toBeVisible();
+    
+   }
+}
---
+++ tests/testdata/Getbyrole.spec.js
@@ -0,0 +1,11 @@
+import{test} from '@playwright/test';
+
+import Getbyrole from '../../Pages/Getbyrole';
+
+test("Validate the GetByRole", async({page})=>{
+
+const getbyrole=new Getbyrole(page);
+
+await getbyrole.goto();
+await getbyrole.getbyrolebuttonvalidation();
+})
